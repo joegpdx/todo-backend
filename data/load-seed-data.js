73b896ce947dc7d0e3136e2b0af2e1a5b12af647
joegpdx <joegpdx@gmail.com>
2020-02-26 17:06:02 -0800

    setup-auth

--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -9,16 +9,21 @@ async function run() {
     try {
         await client.connect();
 
+        await client.query(`
+                    INSERT INTO users (email, hash)
+                    VALUES ($1, $2);
+                `,
+        ['cool@cool.com', 'asdfasdfasdf']);
+
         await Promise.all(
             todos.map(todo => {
                 return client.query(`
-                    INSERT INTO todos (task, complete)
-                    VALUES ($1, $2);
+                    INSERT INTO todos (task, complete, user_id)
+                    VALUES ($1, $2, $3);
                 `,
-                [todo.task, todo.complete]);
+                [todo.task, todo.complete, todo.user_id]);
             })
         );
-
         console.log('seed data load complete');
     }
     catch (err) {
